420c19d18a2d31e65b9b1e49ecd4499043322507
mesh-dell <mwaimonicas@gmail.com>
2023-03-14 14:48:17 +0300

    Functionality

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.math.pow
 
+lateinit var etHeight: EditText
+lateinit var etWeight: EditText
+lateinit var btnCalculate: Button
+lateinit var tvResult: TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etHeight = findViewById(R.id.etHeight)
+        etWeight = findViewById(R.id.etWeight)
+        btnCalculate = findViewById(R.id.btnCalculate)
+        tvResult = findViewById(R.id.tvResult)
+
+        btnCalculate.setOnClickListener {
+            val height = etHeight.text.toString().toDouble()
+            val weight = etWeight.text.toString().toDouble()
+            var bmi = weight/ (height/100).pow(2.0)
+            tvResult.text = "Your BMI is "+bmi.toString()+"kg/m2"
+        }
     }
 }
