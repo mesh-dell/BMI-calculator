48df300892e514b650d9153621c3183b01cb2332
mesh-dell <mwaimonicas@gmail.com>
2023-03-15 21:33:14 +0300

    Bug fixes

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import java.math.RoundingMode
+import java.text.DecimalFormat
 import kotlin.math.pow
 
 lateinit var etHeight: EditText
@@ -22,10 +25,22 @@ class MainActivity : AppCompatActivity() {
         tvResult = findViewById(R.id.tvResult)
 
         btnCalculate.setOnClickListener {
-            val height = etHeight.text.toString().toDouble()
-            val weight = etWeight.text.toString().toDouble()
-            var bmi = weight/ (height/100).pow(2.0)
-            tvResult.text = "Your BMI is "+bmi.toString()+"kg/m2"
+            if(etHeight.text.toString().isEmpty() || etWeight.text.toString().isEmpty())
+            {
+                Toast.makeText(this, "Fill empty parameters", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                val height = etHeight.text.toString().toDouble()
+                val weight = etWeight.text.toString().toDouble()
+                val bmi = weight/ (height/100).pow(2.0)
+                //rounding off to 2dp
+                val df = DecimalFormat("#.##")
+                df.roundingMode = RoundingMode.DOWN
+                val bmirounded = df.format(bmi)
+                tvResult.text = "Your BMI is "+bmirounded.toString()+"kg/m^2"
+            }
+
         }
     }
 }
